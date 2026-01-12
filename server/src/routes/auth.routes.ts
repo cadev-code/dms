abdf3c150da802ece830b17056e9b4abfd5b15f4
Carlos Alberto <carlosdev1810@gmail.com>
2026-01-12 17:14:09 -0600

    feat(server): add authMiddleware to '/auth/me' route for user authentication

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,17 @@
-import { Router } from 'express';
+import { type Request, type Response, Router } from 'express';
 import { validateInput } from '../middlewares/validateInput';
 import { loginSchema } from '../schemas/auth.schema';
 import { login } from '../controllers/auth.controllers';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/auth/login', validateInput(loginSchema), login);
+
+router.get('/auth/me', authMiddleware, (req: Request, res: Response) => {
+  res.json({
+    userId: req.userId,
+  });
+});
 
 export default router;
