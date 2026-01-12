a0ed8b26dd87eb6f0311ab1ecb4e788c1e36c3cd
Carlos Alberto <carlosdev1810@gmail.com>
2026-01-12 15:47:18 -0600

    fix(server-auth): change import statement to use 'type' for Request, Response, and NextFunction

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma_client';
 import { AppError } from '../utils/AppError';
 import { compareEncryptedPassword } from '../helpers/compareEncryptedPassword';
