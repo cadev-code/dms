c05d789dadcc2357c15708730d5861c905fbac91
Carlos Alberto <carlosdev1810@gmail.com>
2026-01-12 15:27:50 -0600

    feat(client): wrap DMS component with QueryClientProvider for react-query integration

--- client/src/DMS.tsx
+++ client/src/DMS.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router';
 import { appRouter } from './router/app.router';
 import { GlobalAlert } from './components/dms/GlobalAlert';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './config/queryClient';
 
 export const DMS = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={appRouter} />
       <GlobalAlert />
-    </>
+    </QueryClientProvider>
   );
 };
