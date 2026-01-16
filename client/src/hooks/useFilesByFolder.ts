a3ab7077269eed24a357edd93541bf87b1d40c04
Carlos Alberto <carlosdev1810@gmail.com>
2026-01-16 11:34:56 -0600

    feat(client): add custom hook to fetch files by folderId

---
+++ client/src/hooks/useFilesByFolder.ts
@@ -0,0 +1,22 @@
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import type { Document } from '@/types/document.types';
+import { fetcher } from '@/api/queryHelpers';
+
+interface FilesByFolderResponse {
+  error: null;
+  data: Document[];
+}
+
+export const useFilesByFolder = (folderId: number) => {
+  return useQuery<
+    FilesByFolderResponse,
+    AxiosError<{ message: string; error: string }>
+  >({
+    queryKey: ['files-by-folder', folderId],
+    queryFn: () => fetcher(`/files/folder/${folderId}`),
+    retry: 2,
+    retryDelay: 1000,
+    staleTime: 1000 * 60, // 1 minute
+  });
+};
