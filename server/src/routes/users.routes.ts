5792a78eca3a0b8b86515dd7f0b7dfa85f2c94f7
Carlos Escobedo <carlosdev1810@gmail.com>
2026-01-22 23:49:19 -0600

    feat(server): add requireRole middleware for user retrieval route

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -3,13 +3,15 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 import { createUser, getUsers } from '../controllers/users.controllers';
 import { validateInput } from '../middlewares/validateInput';
 import { createUserSchema } from '../schemas/users.schema';
+import { requireRole } from '../middlewares/requireRole';
 
 const router = Router();
 
-router.get('/users', authMiddleware, getUsers);
+router.get('/users', authMiddleware, requireRole(['SUPER_ADMIN']), getUsers);
 router.post(
   '/users',
   authMiddleware,
+  requireRole(['SUPER_ADMIN']),
   validateInput(createUserSchema),
   createUser,
 );
