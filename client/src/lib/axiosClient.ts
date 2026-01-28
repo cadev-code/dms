ac9068c637f52a196895a0f098ce12badd42ae26
Carlos Alberto <carlosdev1810@gmail.com>
2026-01-28 17:03:48 -0600

    refactor(client): simplify baseURL assignment for clarity

--- client/src/lib/axiosClient.ts
+++ client/src/lib/axiosClient.ts
@@ -1,8 +1,12 @@
 import { queryClient } from '@/config/queryClient';
 import axios from 'axios';
 
+const baseURL = import.meta.env.DEV
+  ? 'http://localhost:8080'
+  : import.meta.env.VITE_API_URL || 'http://localhost:8080';
+
 export const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL,
   withCredentials: true, // Incluir cookies en las solicitudes
 });
 
