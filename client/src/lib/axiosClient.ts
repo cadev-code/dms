6fe7fa9d1f34d562e68230a21e0fb7aa6c7833d3
Carlos Alberto <carlosdev1810@gmail.com>
2026-02-04 16:19:24 -0600

    fix(axiosClient): increase timeout for POST and DELETE requests to 60 seconds

--- client/src/lib/axiosClient.ts
+++ client/src/lib/axiosClient.ts
@@ -20,9 +20,9 @@ axiosClient.interceptors.request.use((config) => {
   } else if (config.method === 'get') {
     config.timeout = 10000; // 10 segundos para GETs normales
   } else if (config.method === 'post' || config.method === 'put') {
-    config.timeout = 15000; // 15 segundos para POST/PUT
+    config.timeout = 60000; // 60 segundos para POST/PUT
   } else if (config.method === 'delete') {
-    config.timeout = 15000; // 15 segundos para POST/PUT
+    config.timeout = 60000; // 60 segundos para POST/PUT
   }
 
   return config;
